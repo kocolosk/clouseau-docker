06386ba329e967067b8cd47a3ae9803f438f625b
Francis Filion <francis.filion@1stincidentreporting.com>
2021-12-20 14:06:44 -0500

    Update to clouseau-2.20.1

--- clouseau.sh
+++ clouseau.sh
@@ -4,7 +4,6 @@ exec -c "java -server \
     -Xmx2G \
     -Dsun.net.inetaddr.ttl=30 \
     -Dsun.net.inetaddr.negative.ttl=30 \
-    -Dlog4j.configuration=file:/opt/couchdb-search/etc/log4j.properties \
     -XX:OnOutOfMemoryError="kill -9 %p" \
     -XX:+UseConcMarkSweepGC \
     -XX:+CMSParallelRemarkEnabled \
